67b760b120e52e3aef09cb194dc8b4b93de05b16
Jun Lee <82641992+Jun3-dev@users.noreply.github.com>
2022-05-17 17:01:12 +1000

    Update sketch_may10a.ino
    Added Flashing LED Lights, which you can press a hold a button on a bread board to activate

--- sketch_may10a/sketch_may10a.ino
+++ sketch_may10a/sketch_may10a.ino
@@ -1,12 +1,33 @@
+int button_state = 0;
+
 void setup() {
   // put your setup code here, to run once: 
   Serial.begin(9600);
   Serial.println("Hello, World!");
 
+  //Telling code where to look to find pin
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D0, OUTPUT);
+  pinMode(D2, INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello again, World!");
-  
+
+  //Read pin D2 State
+  Serial.println(digitalRead(D2));
+  button_state = digitalRead(D2);
+  if (button_state == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(D0, HIGH);
+    delay(500);
+    digitalWrite(D0, LOW);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(D0, LOW);
+  }
 }
